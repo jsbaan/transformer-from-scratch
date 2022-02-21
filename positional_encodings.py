35c1a9e9ea2dfd81f7d08aca8d1813c46e32d68d
Joris Baan <jsbaan@gmail.com>
2022-02-21 08:53:59 +0100

    small naming refactor

--- positional_encodings.py
+++ positional_encodings.py
@@ -19,20 +19,20 @@ class SinusoidEncoding(torch.nn.Module):
         super().__init__()
 
         # Create matrix of [SeqLen, HiddenDim] representing the positional encoding for max_len inputs
-        pe = torch.zeros(max_len, hidden_dim)
+        pos_embed = torch.zeros(max_len, hidden_dim)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, hidden_dim, 2).float() * (-math.log(10000.0) / hidden_dim))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0)
+        pos_embed[:, 0::2] = torch.sin(position * div_term)
+        pos_embed[:, 1::2] = torch.cos(position * div_term)
+        pos_embed = pos_embed.unsqueeze(0)
 
         # register_buffer => Tensor which is not a parameter, but should be part of the modules state.
         # Used for tensors that need to be on the same device as the module.
         # persistent=False tells PyTorch to not add the buffer to the state dict (e.g. when we save the model)
-        self.register_buffer("pe", pe, persistent=False)
+        self.register_buffer("pos_embed", pos_embed, persistent=False)
 
     def forward(self, x):
-        x = x + self.pe[:, : x.size(1)]
+        x = x + self.pos_embed[:, : x.size(1)]
         return x
 
 
