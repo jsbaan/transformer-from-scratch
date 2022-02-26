6f570c86b7957f237364ddf2bd5e994391240064
Joris Baan <jsbaan@gmail.com>
2022-02-26 18:30:54 +0100

    add unit test for predicting bos token with 1 decoder layer

--- decoder.py
+++ decoder.py
@@ -23,6 +23,7 @@ class TransformerDecoder(nn.Module):
         num_layers: int,
         vocab_size: int,
         dropout_p: float,
+        tie_output_to_embedding: bool = True
     ):
         super().__init__()
 
@@ -37,8 +38,10 @@ class TransformerDecoder(nn.Module):
             ]
         )
         self.output_layer = nn.Linear(hidden_dim, vocab_size, bias=False)
+
         # Note: a linear layer multiplies the input with a transpose of the weight matrix, so no need to do that here.
-        self.output_layer.weight = nn.Parameter(self.embed.weight)
+        if tie_output_to_embedding:
+            self.output_layer.weight = nn.Parameter(self.embed.weight)
         self.softmax = nn.Softmax(dim=-1)
 
     def _reset_parameters(self):
@@ -95,12 +98,15 @@ class TransformerDecoderBlock(nn.Module):
         src_padding_mask: Optional[torch.BoolTensor] = None,
         future_mask: Optional[torch.BoolTensor] = None,
     ):
+
         """
-        x is of shape(batch_size, decoded_sequence_length, hidden_dim)
-        encoder_hidden_states is of shape(batch_size, src_sequence_length, hidden_dim)
+        :param x: (batch_size, decoded_sequence_length, hidden_dim)
+        :param encoder_hidden_states: (batch_size, src_sequence_length, hidden_dim)
+        :param src_padding_mask: (batch_size, src_sequence_length)
+        :param future_mask: (tgt_sequence_length, tgt_sequence_length)
+        :return:
         """
         # Self attention (with future masking during training)
-
         output = self.dropout1(self.self_mha.forward(x, future_mask=future_mask))
         x = self.layer_norm1(x + output)
 
@@ -121,7 +127,7 @@ class TransformerDecoderBlock(nn.Module):
 
 
 class TestTransformerDecoder(unittest.TestCase):
-    def test_transformer_decoder_inference(self):
+    def test_one_layer_transformer_decoder_inference(self):
         """
         Test two forward passes, simulating two inference decoding steps
         """
@@ -131,13 +137,17 @@ class TestTransformerDecoder(unittest.TestCase):
         np.random.seed(seed)
 
         with torch.no_grad():
-            batch_size, src_seq_len, hidden_dim, vocab_size = 2, 10, 512, 2000
+            batch_size, src_seq_len, hidden_dim, vocab_size, num_layers, num_heads = (
+                2,
+                10,
+                512,
+                2000,
+                1,
+                8,
+            )
 
             # Prepare fake encoder hidden states and padding masks
-            # TODO is it required to scale encoder hidden states with this factor?
-            encoder_output = torch.randn(
-                (batch_size, src_seq_len, hidden_dim)
-            ) * math.sqrt(hidden_dim)
+            encoder_output = torch.randn((batch_size, src_seq_len, hidden_dim))
             src_padding_mask = torch.BoolTensor(
                 [[1, 1, 1, 1, 1, 1, 1, 1, 0, 0], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
             )
@@ -148,7 +158,7 @@ class TestTransformerDecoder(unittest.TestCase):
                 hidden_dim=hidden_dim,
                 ff_dim=2048,
                 num_heads=8,
-                num_layers=2,
+                num_layers=1,
                 dropout_p=0.1,
                 vocab_size=vocab_size,
             )
@@ -156,8 +166,9 @@ class TestTransformerDecoder(unittest.TestCase):
             decoder.eval()
 
             # Prepare decoder input, mask, perform a decoding step, take the argmax over the softmax of the last token
+            bos_token_id = 10
             # and iteratively feed the input+prediction back in.
-            decoder_input = torch.IntTensor([[0], [0]])
+            decoder_input = torch.IntTensor([[bos_token_id], [bos_token_id]])
             future_mask = None
             for i in range(3):
                 decoder_output = decoder(
@@ -175,9 +186,67 @@ class TestTransformerDecoder(unittest.TestCase):
                 self.assertEqual(decoder_output.shape, (batch_size, i + 1, vocab_size))
                 # softmax entropy should not be 0
                 self.assertEqual(torch.any(decoder_output == 1), False)
-                # token predictions should not all be 0
-                # TODO this is weird, I expect random token indices here
-                self.assertEqual(torch.all(decoder_input == 0), False)
+                # With only one decoder layer the predicted tokens will always be the input token ids
+                # This happens only when the final linear transformation is tied (the transpose of the embedding matrix)
+                # Maybe because the input embedding is barely transformed, and dot product between the final decoder
+                # hidden state with the input embedding vector is the highest because they are still very similar
+                # Scaling up the memory states also avoid this behavior, probably because the vectors change
+                # sufficiently to randomize the argmax again.
+                self.assertEqual(torch.all(decoder_input == bos_token_id), True)
+
+    def test_multi_layer_transformer_decoder_inference(self):
+        """
+        Test two forward passes, simulating two inference decoding steps
+        """
+        seed = 0
+        torch.manual_seed(seed)
+        random.seed(seed)
+        np.random.seed(seed)
+
+        with torch.no_grad():
+            batch_size, src_seq_len, hidden_dim, vocab_size = 2, 10, 512, 2000
+
+            # Prepare fake encoder hidden states and padding masks
+            encoder_output = torch.randn((batch_size, src_seq_len, hidden_dim))
+            src_padding_mask = torch.BoolTensor(
+                [[1, 1, 1, 1, 1, 1, 1, 1, 0, 0], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+            )
+
+            # Initialize the decoder, perform xavier init and set to evaluation mode
+            decoder = TransformerDecoder(
+                embedding=torch.nn.Embedding(vocab_size, hidden_dim),
+                hidden_dim=hidden_dim,
+                ff_dim=2048,
+                num_heads=8,
+                num_layers=6,
+                dropout_p=0.1,
+                vocab_size=vocab_size,
+            )
+            decoder._reset_parameters()
+            decoder.eval()
+
+            # Prepare decoder input, mask, perform a decoding step, take the argmax over the softmax of the last token
+            bos_token_id = 10
+            # and iteratively feed the input+prediction back in.
+            decoder_input = torch.IntTensor([[bos_token_id], [bos_token_id]])
+            future_mask = None
+            for i in range(3):
+                decoder_output = decoder(
+                    decoder_input,
+                    encoder_output,
+                    src_padding_mask=src_padding_mask,
+                    future_mask=future_mask,
+                )
+                predicted_tokens = torch.argmax(
+                    decoder_output[:, -1, :], dim=-1
+                ).unsqueeze(1)
+                decoder_input = torch.cat((decoder_input, predicted_tokens), dim=-1)
+                future_mask = construct_future_mask(decoder_input.shape[1])
+
+                self.assertEqual(decoder_output.shape, (batch_size, i + 1, vocab_size))
+                # softmax entropy should not be 0
+                self.assertEqual(torch.any(decoder_output == 1), False)
+                self.assertEqual(torch.all(decoder_input == bos_token_id), False)
 
 
 if __name__ == "__main__":
